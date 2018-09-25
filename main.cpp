000a7b4c881645c7721f26fb80ae48d354b9a787
Eperez8 <eperez8@ecc-jaguar.csuchico.edu>
2018-09-24 17:01:29 -0700

    LazyHoroscope

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ string first;
 
 string last;
 
+int first,last;
+
 
 
 //get user input
@@ -24,5 +26,21 @@ cout<<"Welcome, "<<first[0]<<"."<<last[0]<<"."<<" here is your fortune..."<<endl
 //tell fortune
 cout<<"Your Lucky Number is "<<first.length()<<endl;
 
+cin>> first;
+// evaluate lower case first and last vowel
+isLowercaseVowel = (index == 'a' || index == 'e' || index == 'i' || index == 'o' || c == 'u');
+
+    // evaluates if first and last  is an uppercase vowel
+    isUppercaseVowel = ( index == 'A' || index == 'E' || index == 'I' || index == 'O' || index == 'U');
+
+    // evaluates if either isLowercaseVowel or isUppercaseVowel is true
+    if (first)
+        cout << index << " you are destined to be famous.";
+     {
+
+     else(cout<< index << " you have already met your true love."
+ }
+
+ cout(cout<<have a good day!<<endl;
   return 0;
 }
